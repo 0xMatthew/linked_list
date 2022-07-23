a37eabe061b00dd9e0e7c3b0df0ac356df170d25
Matthew Yaeger <matthewcyaeger@gmail.com>
2022-07-23 03:34:50 -0500

    simple linked list contained in one cpp file

--- linked_list.h
+++
@@ -1,7 +0,0 @@
-#ifndef LINKED_LIST_H
-#define LINKED_LIST_H
-class Linked_List{
-    public:
-    void test_print();
-};
-#endif
--- main.cpp
+++ main.cpp
@@ -1,7 +1,43 @@
-#include "linked_list.h"
+#include <iostream>
+class node{
+    public:
+        int data;
+        node * next;
+};
+
+void traverse_node(int n, node *starting_node){
+    node * tmp = starting_node;
+    int i = 0;
+    while(tmp->next && i < n){
+        std::cout << tmp->data << ", ";
+        tmp = tmp->next;
+        i++;
+    }
+}
+
+void push_element_end(int data, node *starting_node){
+    node *cur_node = new node;
+    cur_node->data = data;
+    cur_node->next = starting_node->next;
+    starting_node->next = cur_node; 
+}
 
 void main(){
-    Linked_List linked_list1;
-    linked_list1.test_print();
+
+
+    node *head = new node;
+    node *second = new node;
+    node *third = new node;
+
+    head->data = 1;
+    head->next = second;
+
+    second->data = 2;
+    second->next = third;
+
+    third->data = 3;
+    
+    push_element_end(69, third);
+    traverse_node(4, head);
 
 }
